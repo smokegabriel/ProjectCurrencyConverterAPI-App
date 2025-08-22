f5bb39d451f24035b9e506235148715213b2bc16
uept42-user <uept42-user@aluno.pr.senac.br>
2025-08-22 11:56:02 -0300

    Importa os métodos do retrofit

--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
@@ -1,4 +1,11 @@
 package com.example.projectcurrencyconverterapi;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface ExchangeRateService {
+    /**
+     * Método para obter taxas de câmbio recentes
+     * **/
 }
