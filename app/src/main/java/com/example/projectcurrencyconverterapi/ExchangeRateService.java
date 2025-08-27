406bd110befef23fad43c458a4198f8b42b9e86a
uept42-user <uept42-user@aluno.pr.senac.br>
2025-08-27 09:16:01 -0300

    Adiciona uma Call e substitui apiKey e baseCurrency

--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
@@ -1,3 +1,4 @@
+
 package com.example.projectcurrencyconverterapi;
 
 import retrofit2.Call;
@@ -8,4 +9,12 @@ public interface ExchangeRateService {
     /**
      * Método para obter taxas de câmbio recentes
      * **/
+
+    $GET("/v6/{apiKey}/latest/{baseCurrency}")
+        Call<ExchangeRatesResponse> getExchangeRates(
+        // Substitui {apiKey} na URL com valor fornecido
+        @Path("apiKey") String apiKey,
+        // Substitui {baseCurrency} na URL pelo valor fornecido
+        @Path("baseCurrency") String baseCurrency
+        );
 }
