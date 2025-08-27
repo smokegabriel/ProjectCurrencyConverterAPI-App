6a532381fa438edad60bfd1b4872382653c43751
uept42-user <uept42-user@aluno.pr.senac.br>
2025-08-27 10:13:54 -0300

    Importa as bibliotecas do Retrofit, requisita a URL base da API que fornece as taxas de câmbio e cria o método que retorna uma instância para realizar chamadas à API

--- app/src/main/java/com/example/projectcurrencyconverterapi/RetrofitClient.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/RetrofitClient.java
@@ -1,5 +1,25 @@
 // Classe  responsável por configurar e fornecer uma instância retrofit
 package com.example.projectcurrencyconverterapi;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import com.example.projectcurrencyconverterapi.ExchangeRateService;
 public class RetrofitClient {
+    // URL base da API que fornece taxas de câmbio
+
+    private static final String BASE_URL = "https://v6.exchangerate-api.com/";
+    // Instância do Retrofit que será usada para fazer as requisições
+
+    private static Retrofit retrofit = null;
+
+    /*Método estático que retorna uma instância do serviço de taxa de câmvio
+     *@return Uma instância de ExchangeRateService para realizar chamadas à API
+     */
+
+    public static ExchangeRateService getInstance(){
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        }
+        return retrofit.create(ExchangeRateService.class);
+    }
 }
