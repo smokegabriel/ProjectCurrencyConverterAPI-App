fbffbc986934ae389676d77bc3c58826f0af0a86
uept42-user <uept42-user@aluno.pr.senac.br>
2025-08-27 09:42:47 -0300

    Cria um método para acessar as taxas de conversão e retorna o mapa contendo as taxas de conversão armazenadas

--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
@@ -10,7 +10,7 @@ public interface ExchangeRateService {
      * Método para obter taxas de câmbio recentes
      * **/
 
-    $GET("/v6/{apiKey}/latest/{baseCurrency}")
+    @GET("/v6/{apiKey}/latest/{baseCurrency}")
         Call<ExchangeRatesResponse> getExchangeRates(
         // Substitui {apiKey} na URL com valor fornecido
         @Path("apiKey") String apiKey,
--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRatesResponse.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRatesResponse.java
@@ -1,5 +1,20 @@
 //Modelo de dados (DTO - Data Transfer Object) que representa a resposta json da API
 package com.example.projectcurrencyconverterapi;
 
+//Importa a classe Map da biblioteca Java, usada para armazenar pares de chave-valor
+import java.util.HashMap;
+import java.util.Map;
 public class ExchangeRatesResponse {
+    //Declara um mapa que armazena taxas de conversão em moedas.
+    // A chave (String) representa o código da moeda (ex.: "USD", "EUR"), e o valor (Double) representa a taxa de conversão para essa moeda.
+    private Map<String, Double> conversion_rates = new HashMap<>();
+
+    //Método público para acessar as taxas de conversão
+    //Retorna o mapa contendo as taxas de conversão armazenadas.
+
+    public Map<String,Double> getConversionRates(){
+        return conversion_rates;
+    }
+
 }
+
